44cd39f950d74c312cd88d56adef14fcecc37db1
Elegance239 <elegancework239@gmail.com>
2023-04-29 14:38:25 +0800

    More misc changes.

--- kubejs/client_scripts/ponderjs/jei.js
+++ kubejs/client_scripts/ponderjs/jei.js
@@ -1,5 +1,11 @@
 onEvent('jei.add.items', event => {
-    event.add(Item.of('thermal:laser_diode'))
-    event.add(Item.of('thermal:ruby'))
-    event.add(Item.of('thermal:sapphire'))
-  })
+  event.add(Item.of('thermal:laser_diode'))
+  event.add(Item.of('thermal:ruby'))
+  event.add(Item.of('thermal:sapphire'))
+
+})
+
+onEvent('jei.hide.items', event => {
+  event.hide(Item.of('mm:osmium_drill_controller'))
+  event.hide(Item.of('mm:osmium_drill_laser_input_port'))
+})
