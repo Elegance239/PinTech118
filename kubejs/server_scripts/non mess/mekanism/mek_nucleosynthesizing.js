e12317c394ed7d7c578e5b632c64038202cbd4df
Elegance239 <elegancework239@gmail.com>
2023-05-12 20:29:48 +0800

    Small Addition

---
+++ kubejs/server_scripts/non mess/mekanism/mek_nucleosynthesizing.js
@@ -0,0 +1,12 @@
+
+onEvent('recipes', event => {
+    event.custom(
+        {
+            "type": "mekanism:nucleosynthesizing",
+            "duration": 1000,
+            "gasInput": { "amount": 35, "gas": "mekanism:antimatter" },
+            "itemInput": { "ingredient": { "item": "mekanism:elite_control_circuit" }},
+            "output": { "item": "mekanism:ultimate_control_circuit" }
+        }
+    )
+})
